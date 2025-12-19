5a6431b992828aa3148d646ca20478cdd290169d
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-19 04:01:06 +0100

    add: initialize myLeads array with default values and render leads to the UI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const inputEl = document.getElementById('input-el')
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 
@@ -7,7 +7,9 @@ const ulEl = document.getElementById('ul-el')
 inputBtn.addEventListener('click', () => {
     const record = inputEl.value
     myLeads.push(record)
-    for (lead of myLeads) {
-        console.log(lead)
-    }
 })
+
+
+for (lead of myLeads) {
+    ulEl.innerHTML += `<li>${lead}</li>`
+}
