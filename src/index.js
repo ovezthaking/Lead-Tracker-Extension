c6911be5ac3c2879d71ffc37fabe757aa01b6df3
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-20 01:09:59 +0100

    fix: ensure initial lead is set in localStorage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let myLeads = []
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 
+localStorage.setItem('myLeads', 'www.examplelead.com')
 
 inputBtn.addEventListener('click', () => {
     const record = inputEl.value
