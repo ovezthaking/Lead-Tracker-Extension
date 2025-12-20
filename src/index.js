5ea9e039a1f842b69970485a126a4b9ddbcc112a
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-20 02:16:54 +0100

    refactor: consolidate rendering functions and improve lead management

--- src/index.js
+++ src/index.js
@@ -7,47 +7,13 @@ const deleteBtn = document.getElementById('delete-btn')
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLocalLeads()
+    render(myLeads)
 }
 
-deleteBtn.addEventListener('dblclick', () => {
-    myLeads = []
-    localStorage.clear()
-    renderLocalLeads()
-})
-
-inputBtn.addEventListener('click', () => {
-    const record = inputEl.value
-    myLeads.push(record)
-    renderLead()
-    inputEl.value = ''
-    localStorage.setItem('myLeads', JSON.stringify(myLeads))
-})
-
-function renderLead() {
-    if (inputEl.value){
-        let listItem = `
-            <li>
-                <a href='${inputEl.value}' target='_blank'>
-                    ${inputEl.value}
-                </a>
-            </li>
-        `
-
-        // for (lead of myLeads) {
-        //     listItems += 
-        //     // const li = document.createElement('li')
-        //     // li.textContent = lead
-        //     // ulEl.append(li)
-        // }
-
-        ulEl.innerHTML += listItem
-    }
-}
-
-function renderLocalLeads() {
+function render(leads) {
     let listItems = ''
-    for (lead of myLeads) {
+    
+    for (lead of leads) {
         listItems += `
             <li>
                 <a target='_blank' href='${lead}'>
@@ -58,3 +24,17 @@ function renderLocalLeads() {
     }
     ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener('dblclick', () => {
+    myLeads = []
+    localStorage.clear()
+    render(myLeads)
+})
+
+inputBtn.addEventListener('click', () => {
+    const record = inputEl.value
+    myLeads.push(record)
+    render(myLeads)
+    inputEl.value = ''
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+})
